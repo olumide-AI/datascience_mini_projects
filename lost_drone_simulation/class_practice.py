b501f76ae502d067b6a92d8612a795acd479dded
olumide-AI <olumidekolawole26@gmail.com>
2025-04-18 01:42:00 -0700

    just changed some variables for experiments

--- lost_drone_simulation/class_practice.py
+++ lost_drone_simulation/class_practice.py
@@ -4,12 +4,12 @@ class Employee:
     #intializer / constructor
     # self lets us work with all instance e.g 
     # self will pass emp_1 to all class variable
-    def __init__ (self, first, last, pay):
+    def __init__ (self, firstname, last, pay):
         #set instance variables 
-        self.first = first
-        self.last = last
+        self.first = firstname
+        self.lastname = last
         self.pay = pay
-        self.email = first + "." + last + "@company.com"
+        self.email = firstname + "." + last + "@company.com"
     
     #Each method in a class, automatically takes the instance as the 1st arg
     # Don't forget self in method declaration/parameter
