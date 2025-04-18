ab391cb6dee9083f5475eeb4415092e9c705cd43
olumide-AI <olumidekolawole26@gmail.com>
2025-04-18 04:10:32 -0700

    the class creation

--- pet_mood_tracker/pet.py
+++ pet_mood_tracker/pet.py
@@ -1,3 +1,4 @@
+import random
 class Pet:
     def __init__(self, name, mood):
         # Start your attributes here
@@ -6,8 +7,11 @@ class Pet:
         
 
     def change_mood(self):
-        # This method will be used later
-        pass
+        # Change self.mood by either +1 or -1
+        # If mood is a number, use random.choice to decide
+        # Then update self.mood
+        # mood is a number when 1 is low and 10 is higher
+        self.mood += random.choice([-1,1])
 
-pet_buddy = Pet("Buddy","happy")
-print(pet_buddy)
+pet_buddy = Pet("Buddy",5)
+print(pet_buddy.change_mood())
